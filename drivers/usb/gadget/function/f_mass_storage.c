aaab348dd73ae5a8462de023b5bc2438fca03f21
Marcelo Pires <thesyncim@gmail.com>
2016-12-02 17:33:33 +0000

    comment unused

--- drivers/usb/gadget/function/f_mass_storage.c
+++ drivers/usb/gadget/function/f_mass_storage.c
@@ -2872,7 +2872,7 @@ static DEVICE_ATTR_RW(file);
 static DEVICE_ATTR_RW(cdrom);
 static DEVICE_ATTR(perf, 0644, fsg_show_perf, fsg_store_perf);
 
-static struct device_attribute dev_attr_ro_cdrom = __ATTR_RO(ro);
+//static struct device_attribute dev_attr_ro_cdrom = __ATTR_RO(ro);
 static struct device_attribute dev_attr_file_nonremovable = __ATTR_RO(file);
 
 
